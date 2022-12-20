b2f495fe208a84e2e8fb473aca099a3a19122c42
Jeffrey <jeffrey.shively@hashicorp.com>
2022-12-20 11:26:00 -0500

    Initial

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "username" {
+  type        = string
+  description = "Username to be created in userpass"
+}
+
+variable "password" {
+  type        = string
+  description = "Password to be created for the user in userpass"
+}
